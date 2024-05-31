68858256df44a9be48ad63a7bdb5a9d0098e5073
Durgesh Singh <durgeshownlab@gmail.com>
2024-06-01 00:23:56 +0530

    app.use configure like cookie parse and cors etc

--- src/app.js
+++ src/app.js
@@ -1,5 +1,31 @@
 import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
 const app=express();
+
+// for configuring the server for cross origin resource sharing
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+// for allowing out server to accept the json data 
+app.use(express.json({
+    limit: '16kb'
+}))
+
+// for url encoding
+app.use(express.urlencoded({
+    extended: true
+}))
+
+// for locating the public directory to acccess the files and images
+app.use(express.static("public"))
+
+// for confifuring the cookie parser in the server 
+app.use(cookieParser())
+
+
 
 export { app }
