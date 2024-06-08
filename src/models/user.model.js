6cc6db099091a3fa59c0d04393969670aac6e5dc
Durgesh Singh <durgeshownlab@gmail.com>
2024-06-08 12:50:27 +0530

    user and video schema and jwt and bycrypt implemented

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose, {Schema, Types} from "mongoose";
+import jwt, { sign } from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
 
 const userSchema = new Schema(
     {
@@ -49,6 +51,45 @@ const userSchema = new Schema(
     }
 
 );
+
+userSchema.pre('save', async function(next){
+
+    if(!this.isModified('password')) return next()
+
+    this.password=bcrypt.hash(this.password, 10)
+    next()
+})
+
+userSchema.methods.isPaaswordCorrect=async function(password){
+    return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessToken=function(){
+    jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.userName,
+            fullName: this.fullName,
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+userSchema.methods.generateRefreshToken=function(){
+    jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
 
 export const User = mongoose.model("User", userSchema);
 
--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,12 +1,47 @@
-import mongoose, {Schema} from "mongoose";
+import mongoose, {Schema} from 'mongoose';
+import mongooseAggregatePaginate from 'mongoose-aggregate-paginate-v2';
 
-const videoSchema = new Schema(
+const videoSchema=new Schema(
     {
-        
+        videoFile: {  //cloudinery url
+            type: String,
+            required: true,
+        },
+        thumbnail: {  //cloudinery url
+            type: String,
+            required: true,
+        },
+        owner: {
+            type: Schema.Types.ObjectId,
+            ref: 'User',
+        },
+        title: {
+            type: String,
+            required: true,
+        },
+        description: {
+            type: String,
+            required: true,
+        },
+        duration: {
+            type: Number,
+            required: true,
+        },
+        views: {
+            type: Number,
+            default: 0,
+        },
+        isPublished: {
+            type: Boolean,
+            default: true,
+        }
+
     },
     {
-        timestamps: true
+        timestamps: true,
     }
 );
 
-export const Video=mongoose.model("Video", videoSchema);
+videoSchema.plugin(mongooseAggregatePaginate);
+
+export const Video=mongoose.model('Video', videoSchema)
