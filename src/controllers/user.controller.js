6abc2049eb589cf701edea1c1f332607842c6e70
Durgesh Singh <durgeshownlab@gmail.com>
2024-06-15 00:21:39 +0530

    aggrigate pipleline added for user channe; profile

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -359,6 +359,79 @@ const updateUserCoverImage=asyncHandler(async (req, res)=>{
 });
 
 
+// controller for get user channel profile
+const getUserChannelProfile = asyncHandler(async (req, res)=>{
+   const {username}=req.body;
+
+   if(!username?.trim()) {
+      throw new ApiError(400, 'username is missing')
+   }
+
+   const channel = await User.aggregate([
+      {
+         $match: {
+            username: username?.toLowerCase()
+         }
+      },
+      {
+         $lookup: {
+            from: "subscriptions",
+            localField: "_id",
+            foreignField: "channel",
+            as: "subscribers"
+         }
+      },
+      {
+         $lookup: {
+            from: "subscriptions",
+            localField: "_id",
+            foreignField: "subscriber",
+            as: "subscribedTo"
+         }
+      },
+      {
+         $addFields: {
+            subscribersCount: {
+               $size: "$subscribers"
+            },
+            channelSubscribedToCount: {
+               $size: "$subscribedTo"
+            },
+            isSubscribed: {
+               $cond: {
+                  if: {$in: [req.user?._id, "$subscribedTo"]},
+                  then: true,
+                  else: false
+               }
+            }
+         }
+      },
+      {
+         $project: {
+            fullName: 1,
+            username: 1,
+            subscribersCount: 1,
+            channelSubscribedToCount: 1,
+            isSubscribed: 1,
+            avatar: 1,
+            coverImage: 1,
+            email: 1
+         }
+      }
+   ]);
+
+   if(!channel?.length){
+      throw new ApiError(404, "Channel does not exists")
+   }
+
+   return res 
+   .status(200)
+   .json(
+      new ApiResponse(200, channel[0], 'User channel fatched successfully')
+   )
+
+});
+
 export {
    registerUser, 
    loginUser, 
@@ -366,5 +439,8 @@ export {
    refreshAccessToken, 
    changeCurrentPassword, 
    getCurrentUser,
-   updateAccountDetails
+   updateAccountDetails,
+   updateUserAvatar,
+   updateUserCoverImage,
+   getUserChannelProfile
 }
